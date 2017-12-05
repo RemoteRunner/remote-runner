343922c8b39b8f99cf6298224426bb095725d26f
Vladuslava <tischenko.vlada@gmail.com>
2017-12-05 08:43:33 +0200

    commit

--- client/src/service/api.service.js
+++ client/src/service/api.service.js
@@ -1,5 +1,5 @@
-//const DOMAIN = 'https://rr-test-vlada.herokuapp.com';
-const DOMAIN = 'http://localhost:5000';
+const DOMAIN = 'https://rr-test-vlada.herokuapp.com';
+//const DOMAIN = 'http://localhost:5000';
 
 let myHeaders = new Headers();
 myHeaders.append('Access-Control-Request-Headers','*');
@@ -22,13 +22,13 @@ class ApiService {
 
   register(newUser) {
   	let params = {method: 'POST', body: JSON.stringify(newUser), mode: 'cors',headers: myHeaders};
-  	
+
   	return fetch(new Request(DOMAIN + '/api/register', params))
   }
 
   updateUser(user) {
   	let params = {method: 'PUT', body: JSON.stringify(user), mode: 'cors',headers: myHeaders};
-  	
+
   	return fetch(new Request(DOMAIN + '/api/user-settings', params))
   }
 
@@ -53,7 +53,7 @@ class ApiService {
   sendCommand(command) {
   	console.log(command);
   	let params = {method: 'POST', body: JSON.stringify(command), mode: 'cors',headers: myHeaders};
-  	
+
   	return fetch(new Request(DOMAIN + '/api/execute-command-ui', params))
   }
 
